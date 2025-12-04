7a1ddeb47522d63313d7cf717041a1cedea0b150
Wahyu Akhmad Fadillah <Wahyufadil1140@gmail.com>
2025-12-05 01:52:21 +0700

    feat: Update hero section text, standardize button variants, and refine mobile navigation UI.

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -5,7 +5,10 @@ import Image from "next/image";
 export default function HeroSection() {
   return (
     <section className="relative">
-      <div aria-hidden className="absolute inset-0 isolate contain-strict opacity-50">
+      <div
+        aria-hidden
+        className="absolute inset-0 isolate contain-strict opacity-50"
+      >
         <div className="w-140 h-320 -translate-y-87.5 absolute left-30 top-0 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(0,0%,85%,.08)_0,hsla(0,0%,55%,.02)_50%,hsla(0,0%,45%,0)_80%)]" />
         <div className="h-320 absolute left-30 top-0 w-60 -rotate-45 rounded-full bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.06)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)] [translate:5%_-50%]" />
         <div className="h-320 -translate-y-87.5 absolute left-30 top-0 w-60 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" />
@@ -21,9 +24,9 @@ export default function HeroSection() {
             className="
               mb-10
               lg:mb-0
-              lg:absolute 
-              lg:right-15 
-              lg:top-1/2 
+              lg:absolute
+              lg:right-15
+              lg:top-1/2
               lg:-translate-y-1/2
               w-[300px] h-[300px] lg:w-[500px] lg:h-[500px]
             "
@@ -31,12 +34,12 @@ export default function HeroSection() {
 
           {/* Text */}
           <div className="pb-20 lg:pb-1 z-10">
-            <h1 className="max-w-2xl text-balance text-4xl sm:text-5xl font-medium lg:text-6xl">
+            <h1 className="max-w-2xl  text-balance text-4xl sm:text-5xl font-medium lg:text-6xl">
               Plan. Build. Ship.
             </h1>
             <p className="mt-4 max-w-lg text-muted-foreground text-sm sm:text-base lg:text-lg">
-              Kreeasi providing Web, iOS, Android Application Design &
-              Development services proand quality according to your idea.
+              Kreeasi menyediakan solusi web yang profesional dan kualitas
+              terbaik sesuai dengan ide Anda.
             </p>
 
             <div className="mt-8 grid grid-cols-2 md:flex items-center gap-2">
@@ -52,7 +55,7 @@ export default function HeroSection() {
               <Button
                 asChild
                 size="lg"
-                variant="ghost"
+                variant="outline"
                 className="text-sm lg:text-base rounded-full lg:min-w-46 px-5 w-full sm:w-auto"
               >
                 <Link href="#link">
